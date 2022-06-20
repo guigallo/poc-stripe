e309c93c9e6793c0aeb1e1a0778882232bada82e
guigallo <guilherme.gg1@hotmail.com>
2022-06-19 23:39:26 -0300

    configure stripe with dotenv

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 // Set your secret key. Remember to switch to your live secret key in production.
 // See your keys here: https://dashboard.stripe.com/apikeys
-const stripe = require('stripe')(process.env.STRIPE_TOKEN);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // This example uses Express to receive webhooks
 const express = require('express');
